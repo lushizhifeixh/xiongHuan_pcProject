fdcf080514a40c24c036d0c36343069f7bdb2a47
xiongHuan <429307282@qq.com>
2017-05-17 17:23:34 +0800

    登录和注册页面

--- js/funct/header.js
+++ js/funct/header.js
@@ -1,5 +1,6 @@
 // 加载头部
- $("#header_html").load("/html/comm/header.html",function(){//header_html
+$(function(){//ready
+$("#header_html").load("/html/comm/header.html",function(){//header_html	
  	// header_nav
 $(".right_subnav").hover(function(){
 	$(this).find("span").css({
@@ -8,7 +9,6 @@ $(".right_subnav").hover(function(){
 	$(this).find("ul").show().css({
 		background:"#fff"
 	});
-
 },function(){
 	$(this).find("span").css({
 		background:"#fff"
@@ -21,16 +21,85 @@ $(".mynav01").hover(function(){
 },function(){
 	$(this).find(".position").hide();
  });
-// $("#oSpan").hover(function(){
-// 	$(this).next().show();
-// },function(){
-// 	$(this).next().hide();
-// });
 // 吸顶菜单
 // 登录
+$("#login_user").on("click",function(){
+	// 登录块
+	$("#alert_login").css({
+		display:"block",
+		position:"absolute",
+		left:0,
+		right:0,
+		top:0,
+		bottom:0,
+		margin:"auto",
+		width:"400px",
+		height:"400px",
+		zIndex:999
+	});
+	// 大朦板
+	
+	$("html,body").css({
+		height:"100%"
+	});
+	$("<div class='mybg'></div>").appendTo($("body"));
+	$(".mybg").css({
+		background:"#000",
+		width:"100%",
+		height:"100%",
+		position:"absolute",
+		top:0,
+		left:0,
+		opacity:0.4,
+		zIndex:500,
+	});
+	
+})//登录
+//注册
+$("#register_user").on("click",function(){
+	// 登录块
+	$("#alert_register").css({
+		display:"block",
+		position:"absolute",
+		left:0,
+		right:0,
+		top:0,
+		bottom:0,
+		margin:"auto",
+		width:"520px",
+		height:"500px",
+		zIndex:999,
+		background:"#ffff00"
+		
+	});
+	// 大朦板
+	
+	$("html,body").css({
+		height:"100%"
+	});
+	$("<div class='mybg'></div>").appendTo($("body"));
+	$(".mybg").css({
+		background:"#000",
+		width:"100%",
+		height:"100%",
+		position:"absolute",
+		top:0,
+		left:0,
+		opacity:0.4,
+		zIndex:500,
+	});
+	
+})//登录
+//注册
+// function show(){
+// 	 $("#alert").show();
+// 	// var mybg=$("<div></div>");
+// 	// mybg.appendTo($(document));
+	
+// }
 // 注册
 //购物车
 
-
- });//header_html
-
+ 
+});//header_html
+})//ready
